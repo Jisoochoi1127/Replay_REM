32f7009acf5dbb0677d2f1ccbcbbaa04a993e611
Guillaume Etter <35970969+etterguillaume@users.noreply.github.com>
2023-12-15 17:15:00 -0500

    Updated analysis to novel format

--- seqNMF_analyze_results.py
+++ seqNMF_analyze_results.py
@@ -7,33 +7,68 @@ import matplotlib.pyplot as plt
 plt.style.use('plot_style.mplstyle')
 
 import pingouin as pg
+import h5py
 
 #%%
-df = pd.read_csv('../../output_REM/seqScores.csv')
+results_dir = '../../output_REM/'
+resultsList=os.listdir(results_dir)
+data_list = []
+for file_name in resultsList:
+    if file_name.startswith('seqResults_') and file_name.endswith('.h5'): # Only include seqResults, not replay results
+        h5_file = h5py.File(os.path.join(results_dir,file_name))
+        data_list.append( #This will create one list entry per cell
+                {
+                    'state':h5_file['state'][()].decode("utf-8"),
+                    'condition':h5_file['condition'][()].decode("utf-8"),
+                    'mouse':h5_file['mouse'][()].decode("utf-8"),
+                    'S1_numSeqs':h5_file['S1_numSeqs'][()],
+                    'S2_numSeqs':h5_file['S2_numSeqs'][()],
+                    'S1_score':h5_file['S1_score'][()],
+                    'S2_score':h5_file['S2_score'][()],
+                    'S1_pvalue':h5_file['S1_pvalue'][()],
+                    'S2_pvalue':h5_file['S2_pvalue'][()],
+                }
+            )
+
+        # Close files
+        h5_file.close()
+
+df = pd.DataFrame(data_list)
+
+
+df_stats = df
+df_stats.loc[df_stats['S1_pvalue']>0.05,'S1_numSeqs']=0
+df_stats.loc[df_stats['S2_pvalue']>0.05,'S2_numSeqs']=0
+
+#%%
+df_stats=df_stats.melt(id_vars=['state','mouse', 'condition'],value_name='numSeqs',value_vars=['S1_numSeqs', 'S2_numSeqs'],var_name='seqType')
 # %%
 sns.barplot(
-    data=df,
-    y='max_seq_zscore',
+    data=df_stats,
+    y='numSeqs',
     x='condition',
     hue='state',
+    hue_order=['REMpre','wake','REMpost'],
     palette=['C0','C6','C1'],
     errorbar='se',
     capsize=.2
 )
 sns.stripplot(
-    data=df,
-    y='max_seq_zscore',
+    data=df_stats,
+    y='numSeqs',
     x='condition',
     hue='state',
     palette=['C0','C6','C1'],
+    hue_order=['REMpre','wake','REMpost'],
     size=2,
     dodge=True,
     legend=False
 )
 plt.xticks([0,1,2],['Novel', 'Fam.', 'Anxiety'])
-plt.ylabel('Seq. score (z)')
+plt.ylabel('Num. sig.\nsequences')
+plt.xlabel('')
 plt.legend(bbox_to_anchor=(1.1, 1), loc='upper left', borderaxespad=0)
-plt.savefig('../../output_REM/seq_zscore.png')
+plt.savefig('../../output_REM/numSigSequences.png')
 
 #%%STATS
 
