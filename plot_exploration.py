6c3ff8383abc3df07f0557bd3e0084fa08dc5081
Guillaume Etter <etterguillaume@gmail.com>
2024-01-25 14:22:44 -0500

    plotting maze exploration wrt speed thresholds

--- plot_exploration.py
+++ plot_exploration.py
@@ -1,50 +1,73 @@
-#%%
+# %% Imports
 import numpy as np
-import os
 from tqdm import tqdm
 import itertools
-
 import yaml
-import h5py
 from utils.helperFunctions import load_data
 import matplotlib.pyplot as plt
-plt.style.use('plot_style.mplstyle')
 
-#%% Load parameters
-with open('params.yaml','r') as file:
+plt.style.use("plot_style.mplstyle")
+plt.ion()
+
+# %% Load parameters
+with open("params.yaml", "r") as file:
     params = yaml.full_load(file)
 
-np.random.seed(params['seed'])
+np.random.seed(params["seed"])
 
-#%% Define conditions, dataset
-condition_list = ['LTD1','LTD5','HATD1','HATD5']
-mouse_list = ['pv1043','pv1060', 'pv1069', 'pv1191', 'pv1192', 'pv1252', 'pv1254']
+# %% Define conditions, dataset
+condition_list = ["LTD1", "LTD5", "HATD1", "HATD5"]
+mouse_list = ["pv1043", "pv1060", "pv1069", "pv1191", "pv1192", "pv1252", "pv1254"]
+
 # %%
-ct=1
-plt.figure(figsize=(len(mouse_list),len(condition_list)))
-for condition, mouse in tqdm(list(itertools.product(condition_list,
-                                                           mouse_list
-                                                           )),
-                                                           total=len(condition_list)*len(mouse_list)):
+ct = 1
+plt.figure(figsize=(len(mouse_list), len(condition_list)))
+for condition, mouse in tqdm(
+    list(itertools.product(condition_list, mouse_list)),
+    total=len(condition_list) * len(mouse_list),
+):
     try:
-            
-        plt.subplot(len(condition_list),len(mouse_list),ct)
+        plt.subplot(len(condition_list), len(mouse_list), ct)
 
-        data = load_data(mouse=mouse, condition=condition, state='wake',params=params)
-        plt.plot(data['position'][:,0],data['position'][:,1],'C4',linewidth=.2)
-        plt.plot([0,100],[0,0],'k', linewidth=.6)
-        plt.plot([0,100],[15,15],'k', linewidth=.6)
-        plt.plot([0,0],[0,15],'k', linewidth=.6)
-        plt.plot([100,100],[0,15],'k', linewidth=.6)
+        data = load_data(mouse=mouse, condition=condition, state="wake", params=params)
+        plt.plot(data["position"][:, 0], data["position"][:, 1], "C4", linewidth=0.2)
+        plt.plot([0, 100], [0, 0], "k", linewidth=0.6)
+        plt.plot([0, 100], [15, 15], "k", linewidth=0.6)
+        plt.plot([0, 0], [0, 15], "k", linewidth=0.6)
+        plt.plot([100, 100], [0, 15], "k", linewidth=0.6)
 
-        plt.xlim(-1,101)
-        plt.ylim(-80,20)
-        plt.axis('off')
-        plt.title(f'{mouse}-{condition}')
-        ct+=1
+        plt.xlim(-1, 101)
+        plt.ylim(-80, 20)
+        plt.axis("off")
+        plt.title(f"{mouse}-{condition}")
+        ct += 1
     except:
-        print('Missing session')
-        plt.axis('off') 
-    
-plt.savefig('../../output_REM/exploration.pdf')
-# %%
+        print("Missing session")
+        plt.axis("off")
+
+plt.savefig("../../output_REM/exploration.pdf")
+
+# %% Plot maze exploration using params.speed_threshold
+ct = 1
+plt.figure(figsize=(len(mouse_list), len(condition_list)))
+for condition, mouse in tqdm(
+    list(itertools.product(condition_list, mouse_list)),
+    total=len(condition_list) * len(mouse_list),
+):
+    try:
+        plt.subplot(len(condition_list), len(mouse_list), ct)
+
+        data = load_data(mouse=mouse, condition=condition, state="wake", params=params)
+        plt.hist(
+            data["position"][data["velocity"] > params["speed_threshold"], 0], bins=40
+        )
+
+        plt.axis("off")
+        plt.title(f"{mouse}-{condition}")
+        ct += 1
+    except:
+        print("Missing session")
+        plt.axis("off")
+
+plt.tight_layout()
+plt.savefig("../../output_REM/speed_thresholded_explorations.pdf")
