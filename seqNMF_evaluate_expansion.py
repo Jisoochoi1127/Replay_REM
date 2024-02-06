c70d9fe86fca7ac1ee0f992c740478f836d9f0cd
Guillaume Etter <etterguillaume@gmail.com>
2024-02-06 16:31:23 -0500

    added optimization for seqNMF params

--- seqNMF_evaluate_expansion.py
+++ seqNMF_evaluate_expansion.py
@@ -6,7 +6,7 @@ import yaml
 import h5py
 from scipy.stats import skew
 from seqnmf import seqnmf
-from utils.helperFunctions import load_data
+from utils.helperFunctions import load_data, extract_seqReplay_score
 from tqdm import tqdm
 
 plt.style.use("plot_style.mplstyle")
@@ -18,7 +18,7 @@ with open("params.yaml", "r") as file:
 np.random.seed(params["seed"])
 
 # %% Load LT and REM data
-mouse = 'pv1069'
+mouse = 'pv1060'
 condition = 'LTD1'
 data_LT = load_data(mouse=mouse,
                     condition=condition,
@@ -28,30 +28,23 @@ data_REMpost = load_data(mouse=mouse,
                     condition=condition,
                     state='REMpost',
                     params=params)
+
 with h5py.File(os.path.join(params['path_to_output'],"neuron_selection", f'selected_neurons_{condition}_{mouse}.h5'),'r') as f:
     selected_neurons = f['place_cells'][()]
 
 PC_activity = data_LT['binaryData'][:,selected_neurons]
 
-# %% First, let's optimize 'L':
-L_list = [25,50,75,125,150,175,200,225,250]
-L_scores = np.zeros((len(L_list),params['K']))
-
-for i, L in enumerate(tqdm(L_list)):
-    W_LT, H_LT, _, _, _ = seqnmf(
-        PC_activity.T,
-        K=params['K'],
-        L=params['L'],
-        Lambda=params['Lambda'],
-        max_iter=10
-        )
-    L_scores[i,:] = skew(H_LT, axis=1)
-
-# %%  Plot results
-plt.plot(L_list,L_scores)
-
 # %% Create ~20 different compressed/streched templates by interpolation
+W_LT, H_LT, _, _, _ = seqnmf(
+    PC_activity.T,
+    K=params['K'],
+    L=params['L'],
+    Lambda=params['Lambda'],
+    max_iter=10
+    )
 
 # %% For each template, compute matrix multiplication to extract H
+expansionFactorList = [.5,.75,1,1.5,2]
+
 
 # %% Compute
---
+++ seqNMF_optimize_L.py
@@ -0,0 +1,55 @@
+# %% Imports
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import yaml
+import h5py
+from scipy.stats import skew
+from seqnmf import seqnmf
+import itertools
+from utils.helperFunctions import load_data, extract_seqReplay_score
+from tqdm import tqdm
+
+plt.style.use("plot_style.mplstyle")
+
+# %% Params
+with open("params.yaml", "r") as file:
+    params = yaml.full_load(file)
+
+np.random.seed(params["seed"])
+
+# %% Load LT and REM data
+condition = 'LTD1'
+mouse_list = ['pv1043','pv1060', 'pv1069', 'pv1254']
+L_list = [25,50,75,125,150,175,200,225,250,300]
+L_scores = np.zeros((len(L_list),params['K']))
+params['maxIter'] = 5
+
+for mouse in mouse_list:
+    data_LT = load_data(mouse=mouse,
+                    condition=condition,
+                    state='wake',
+                    params=params)
+    data_REMpost = load_data(mouse=mouse,
+                    condition=condition,
+                    state='REMpost',
+                    params=params)
+
+    with h5py.File(os.path.join(params['path_to_output'],"neuron_selection", f'selected_neurons_{condition}_{mouse}.h5'),'r') as f:
+        selected_neurons = f['place_cells'][()]
+
+    PC_activity = data_LT['binaryData'][:,selected_neurons]
+
+    temp_L_scores = np.zeros((len(L_list),params['K']))
+    for i, L in enumerate(tqdm(L_list)):
+        params['L'] = L # Override parameters
+        seqReplay_scores, seqReplay_pvalues, seqReplay_locs = extract_seqReplay_score(data_LT['binaryData'][:,selected_neurons], data_REMpost['binaryData'][:,selected_neurons], params)
+
+        temp_L_scores[i,0] = len(seqReplay_locs[0])
+        temp_L_scores[i,1] = len(seqReplay_locs[1])
+
+    L_scores = np.append(L_scores,temp_L_scores,axis=1)
+
+# %% Save data
+with h5py.File(os.path.join(params['path_to_output'],'optimal_L.h5'),'w') as f:
+    f.create_dataset('L_scores', data=L_scores)
