6e4de78f2a8ebcf3d3b8d07890110e237a5c8668
Guillaume Etter <etterguillaume@gmail.com>
2024-03-04 15:15:09 -0500

    Implementing sliding window analysis

--- utils/bayesian_replay.py
+++ utils/bayesian_replay.py
@@ -1,22 +1,35 @@
+# %% Imports
+import numpy as np
+from numpy import polyfit
 
-
-
-
-
+# %%
 def extract_linear_replay(posterior_probs, params):
-    replay_locs = []
-    replay_score = []
-    replay_jumpiness = []
-    replay_length = []
+    replayLocs = []
+    replayScore = []
+    replayJumpiness = []
+    replayLength = []
 
     # Get shuffled values
-    # for shuffle_i in range(params['numShuffles']):
+    for shuffle_i in range(params['numShuffles']):
+        
+        pass
+        
+
+    currentWindowIdx = np.array([0:params['windowSize']])
+    while True:
+        
+        
+
+        currentWindow = posterior_probs[currentWindowIdx]
+
+        # For each window, compute score, jumpiness, length
+
+        # If 3 scores exceed shuffled surrogate, append index and properties to variables
     
-    # Establish window parameters
+        current_window+=params['stepSize']
+        if current_window[1]>len(posterior_probs):
+            break
 
-    # For each window, compute score, jumpiness, length
+    return replayLocs, replayScore, replayJumpiness, replayLength
 
-    # If 3 scores exceed shuffled surrogate, append index and properties to variables
-    
-
-    return replay_locs, replay_score, replay_jumpiness, replay_length
+# %%
