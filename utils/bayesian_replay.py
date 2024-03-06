cfb91dc7002e6fc0a296c8926d7ebb4ca88f8eb4
Guillaume Etter <etterguillaume@gmail.com>
2024-03-06 14:51:08 -0500

    Completed sliding window linear fit analysis

--- utils/bayesian_replay.py
+++ utils/bayesian_replay.py
@@ -1,10 +1,8 @@
 # %% Imports
 import numpy as np
-from numpy import polyfit, polyval
+from numpy import polyfit
 
-# %% Polyfit analysis
 def linear_fit(time_vec, position):
-
     # Assess replay length
     nanIdx = np.isnan(position)
     portion_window = sum(~nanIdx)/len(time_vec)
@@ -20,58 +18,46 @@ def linear_fit(time_vec, position):
 
     return score, jumpiness, portion_window
 
-# %%
 def extract_linear_replay(posterior_probs, params):
     replayLocs = []
     replayScore = []
     replayJumpiness = []
-    replayLength = []
+    replayPortion = []
 
-    #TODO deal with preallocation, empty windows
+    # Compute actual maximum a posteriori from posterior probabilities, convert to cm
+    actual_map = (np.argmax(posterior_probs,axis=1)+params['spatialBinSize']/2)*params['spatialBinSize']
 
+    # Shuffle posteriors, extract shuffled maps
+    shuffled_maps = np.zeros((params['numShuffles'],len(posterior_probs)))*np.nan
+    for shuffle_i in range(params['numShuffles']):
+        # Shuffle posterior locations
+        shuffled_posteriors = posterior_probs[:,np.random.shuffle(np.arange(posterior_probs.shape[1]))]
+        # Compute argmax on shuffled data
+        shuffled_maps[shuffle_i,:] = (np.argmax(shuffled_posteriors,axis=1)+params['spatialBinSize']/2)*params['spatialBinSize']
+
+    ## Sliding window analysis
+    # Initialize first window
     currentWindowIdx = np.arange(params['windowSize'])
 
-    while True:
-        currentWindow = posterior_probs[currentWindowIdx]
+    while currentWindowIdx[1]<len(posterior_probs):
+        # For each window, compute score, jumpiness, portion replayed
+        actual_score, actual_jumpiness, actual_portion = linear_fit(currentWindowIdx, actual_map[currentWindowIdx])
 
-        # Compute maximum a posteriori, output results in cm for correct speed computations
-        #TODO assert empty/nan frames remain nan values
-        actual_map = (np.argmax(currentWindow,axis=1)+params['spatialBinSize']/2)*params['spatialBinSize']
-
-        # For each window, compute score, jumpiness, length
-        actual_score, actual_jumpiness, actual_length = linear_fit(currentWindowIdx, actual_map)
-
-        
+        # Same for shuffled
+        shuffled_score = np.zeros(params['numShuffles'])
+        shuffled_jumpiness = np.zeros(params['numShuffles'])
+        shuffled_portion = np.zeros(params['numShuffles'])
+        for shuffle_i in range(params['numShuffles']):
+            shuffled_score[shuffle_i], shuffled_jumpiness[shuffle_i], shuffled_portion[shuffle_i] = linear_fit(currentWindowIdx, shuffled_maps[shuffle_i,currentWindowIdx])
     
+        # If 3 scores exceed shuffled surrogate, append index and properties to variables
+        if actual_score>=np.percentile(shuffled_score, 95) and actual_jumpiness<=np.percentile(shuffled_jumpiness,5) and actual_portion>=np.percentile(shuffled_portion,95):
+            replayLocs.append(currentWindowIdx[0])
+            replayScore.append(actual_score)
+            replayJumpiness.append(actual_jumpiness)
+            replayPortion.append(actual_portion)
+        
         currentWindowIdx+=params['stepSize'] # Step forward
 
-        if currentWindowIdx[1]>len(posterior_probs): # If window goes beyond recording
-            break
-
-    # Get shuffled values
-    for shuffle_i in range(params['numShuffles']):
-        #TODO shuffle data here
-        
-        currentWindowIdx = np.array([0:params['windowSize']])
-        while True:
-            currentWindow = posterior_probs[currentWindowIdx]
-
-            actual_map = (np.argmax(currentWindow,axis=1)+params['spatialBinSize']/2)*params['spatialBinSize']
-
-            # For each window, compute score, jumpiness, length
-            shuffled_score, shuffled_jumpiness, shuffled_length = linear_fit(time,actual_map)
-
-
-            current_window+=params['stepSize']
-            if current_window[1]>len(posterior_probs):
-                break
-        
-    # If 3 scores exceed shuffled surrogate, append index and properties to variables
-    significant_replay_events = np.zeros(len(actual_score))
-    for i, time_loc in enumerate(significant_replay_events):
-        if actual_score>=np.percentile(shuffled_score,95) and actual_jumpiness<np.percentile(shuffled_jumpiness,95) and actual_length>np.percentile(shuffled_length,95)
-            replayLocs.append(#TODO)
-            replayScore.append(actual_score[i])
-            replayJumpiness.append(actual_jumpiness[i])
-    return replayLocs, replayScore, replayJumpiness, replayLength
+    return replayLocs, replayScore, replayJumpiness, replayPortion
 
